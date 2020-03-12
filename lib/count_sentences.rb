af9524d50213144f635ab1b9f75ea7c54864e773
Marsha Annon <marshaannon@gmail.com>
2020-03-12 00:10:32 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,10 +27,10 @@ class String
   end
 
   def count_sentences
-      self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
+      self.split(/[.!?]/).reject {|x| x.empty?}.size
     end
 
 
 
-  end
+  
 end
